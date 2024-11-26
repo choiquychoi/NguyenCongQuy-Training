1632d5b13d578f2728435eaeee084bac1b210c35
congquy <quy.nc@qtsoftware.vn>
2024-11-26 14:54:47 +0700

    Learning how to use Const in C#

--- Program.cs
+++ Program.cs
@@ -4,16 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Declare variables
-        int age = 20; // Integer variable
-        string name = "Alice"; // String variable
-        float height = 5.4f; // Float variable
-        bool isGraduated = false; // Boolean variable
+        // Declare constants
+        const float Pi = 3.14f; // Value of Pi
+        const int DaysInWeek = 7; // Number of days in a week
+        const string AppName = "My Application"; // Application name
 
-        // Print values
-        Console.WriteLine("Name: " + name);
-        Console.WriteLine("Age: " + age);
-        Console.WriteLine("Height: " + height);
-        Console.WriteLine("Graduated: " + isGraduated);
+        // Print constants
+        Console.WriteLine("Value of Pi: " + Pi);
+        Console.WriteLine("Number of days in a week: " + DaysInWeek);
+        Console.WriteLine("Application Name: " + AppName);
+
+        // Attempt to modify constant (will cause error)
+        // Pi = 3.1415f; // Error: Cannot assign to 'Pi' because it is a constant
     }
 }
