9e306c18c07f5e44fccd9fec2e624a561ad195ce
congquy <quy.nc@qtsoftware.vn>
2024-12-30 15:46:46 +0700

    Make a first contact with M-V-C

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,17 +15,23 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        // from products.cs model class make a list of products and return it to the view but it hace to return to the home/index view
+
+        var products = new List<Products>
+        {
+            new Products { Id = 1, Name = "Product 1", Price = 10.0m },
+            new Products { Id = 2, Name = "Product 2", Price = 20.0m },
+            new Products { Id = 3, Name = "Product 3", Price = 30.0m },
+            new Products { Id = 4, Name = "Product 4", Price = 40.0m },
+            new Products { Id = 5, Name = "Product 5", Price = 50.0m }
+        };
+        // return the list of products to the home/ index view
+        return View(products);
     }
 
-    public IActionResult Privacy()
+    public IActionResult Details(int id)
     {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        var product = new Products { Id = id, Name = "Product " + id, Price = 10.0m * id };
+        return View(product);
     }
 }
