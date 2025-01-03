bc3b1351454cc07b7795d76b81c409f9813269ec
congquy <quy.nc@qtsoftware.vn>
2025-01-03 14:09:15 +0700

    Create Login admin page

--- Program.cs
+++ Program.cs
@@ -1,9 +1,32 @@
+﻿using practice_C_.Areas.code;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<SessionHelper>();
+
+builder.Services.AddHttpContextAccessor();
+
+
+// Đăng ký các dịch vụ cần thiết
+builder.Services.AddControllersWithViews();
+
+// Đăng ký dịch vụ Session
+builder.Services.AddSession(); // Thêm dòng này
+
+// Đăng ký IHttpContextAccessor (nếu cần)
+builder.Services.AddHttpContextAccessor();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddHttpContextAccessor();
+
 var app = builder.Build();
+
+// Thêm middleware
+app.UseStaticFiles();
+app.UseRouting();
+app.UseSession();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -18,7 +41,17 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseSession();
+
 app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "areas",
+        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+    endpoints.MapDefaultControllerRoute();
+});
 
 app.MapControllerRoute(
     name: "default",
